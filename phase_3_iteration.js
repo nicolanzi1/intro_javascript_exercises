381832adb593541bb4417d8f7570d0b018345220
nicolanzi1 <nicolanzi@gmail.com>
2020-08-28 10:28:38 +0100

    Wrote array and string iteration problems in JS for phase 3

---
+++ phase_3_iteration.js
@@ -0,0 +1,33 @@
+Array.prototype.bubbleSort = function () {
+    let isSorted = false;
+
+    while (!isSorted) {
+        isSorted = true;
+
+        for (let i = 0; i < (this.length - 1); i++) {
+            if (this[i] > this[i + 1]) {
+                [this[i], this[i + 1]] = [this[i + 1], this[i]];
+                isSorted = false;
+            }
+        }
+    }
+
+    return this;
+}
+
+console.log([5, 3, 4, 2, 1].bubbleSort());
+
+
+String.prototype.substrings = function () {
+    substrings = [];
+
+    for (let start = 0; start < this.length; start++) {
+        for (let end = start + 1; end <= this.length; end++) {
+            substrings.push(this.slice(start, end));
+        }
+    }
+
+    return substrings;
+};
+
+console.log("abc".substrings());
